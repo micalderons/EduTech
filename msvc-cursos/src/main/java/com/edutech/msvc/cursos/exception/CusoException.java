3db3e49b8667ced81b395698e2ba884e632ddac4
r1cky-pc-duoc <ri.saavedraf@duocuc.cl>
2025-05-14 11:28:15 -0400

    Merge y implementacion msvc curso

---
+++ msvc-cursos/src/main/java/com/edutech/msvc/cursos/exception/CusoException.java
@@ -0,0 +1,7 @@
+package com.edutech.msvc.cursos.exception;
+
+public class CusoException extends RuntimeException {
+    public CusoException(String message) {
+        super(message);
+    }
+}
---
+++ msvc-cursos/src/main/java/com/edutech/msvc/cursos/services/CursoService.java
@@ -0,0 +1,11 @@
+package com.edutech.msvc.cursos.services;
+
+import com.edutech.msvc.cursos.models.entities.Curso;
+
+import java.util.List;
+
+public interface CursoService {
+    List<Curso> findAll();
+    Curso findById(Long id);
+    Curso save(Curso curso);
+}
