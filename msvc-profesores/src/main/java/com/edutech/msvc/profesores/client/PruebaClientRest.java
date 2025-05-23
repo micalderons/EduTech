6c6b5bbe36e53ebc22b24a7bef89dcf07031204d
Lukas <lukas.armijo@gmail.com>
2025-05-23 13:21:52 -0400

    profesor listo

--- msvc-profesores/src/main/java/com/edutech/msvc/profesores/client/PruebaClientRest.java
+++ msvc-profesores/src/main/java/com/edutech/msvc/profesores/client/PruebaClientRest.java
@@ -4,7 +4,7 @@ import com.edutech.msvc.profesores.models.Pruebas;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-@FeignClient(name = "msvc-curso", url = "localhost:8087/api/v1/Prubas")
+@FeignClient(name = "msvc-prueba", url = "localhost:8087/api/v1/Prubas")
 public interface PruebaClientRest {
     @GetMapping("/{id}")
     Pruebas findById(@PathVariable Long id);
