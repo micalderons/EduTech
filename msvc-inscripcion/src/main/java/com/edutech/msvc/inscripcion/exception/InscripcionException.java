e2d63c0bf4b42b488fe4e88849a6a8aaf204a6b6
r1cky-pc <ri.saavedraf@duocuc.cl>
2025-05-27 21:42:17 -0400

    arreglando esta wuea x2

--- msvc-inscripcion/src/main/java/com/edutech/msvc/inscripcion/exception/InscripcionException.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.msvc.inscripcion.exception;
-
-public class InscripcionException extends RuntimeException {
-    public InscripcionException(String message) {
-        super(message);
-    }
-}
