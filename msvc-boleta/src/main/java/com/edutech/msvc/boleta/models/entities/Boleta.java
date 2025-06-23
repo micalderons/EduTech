b6cd53ed8cfb0d2b656030f932cff8b487d52b28
Benja <bi.gonzalezr@duocuc.cl>
2025-06-22 23:55:11 -0400

    compra y boleta

--- msvc-boleta/src/main/java/com/edutech/msvc/boleta/models/entities/Boleta.java
+++ msvc-boleta/src/main/java/com/edutech/msvc/boleta/models/entities/Boleta.java
@@ -1,22 +1,44 @@
 package com.edutech.msvc.boleta.models.entities;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "boletas")
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Boleta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_boleta")
     private Long idBoleta;
 
+    @Column(name = "hora_boleta", nullable = false)
+    @NotNull(message = "El campo hora boleta no puede estar vacío, con formato 'YYYY-MM-DD HH:mm:ss'")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime horaBoleta;
 
     @Column(nullable = false)
+    @NotNull(message = "El campo total no puede estar vacío")
+    private Integer total;
 
+    @Column(name = "id_curso")
+    @NotNull(message = "El campo id curso no puede estar vacío")
+    private Long idCurso;
 
+    @Column(name = "id_profesor")
+    @NotNull(message = "El campo id profesor no puede estar vacío")
+    private Long idProfesor;
+
+    @Column(name = "id_alumno")
+    private Long idAlumno;
 }
